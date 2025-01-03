40701ea6893c1b28d1e1fe8487c4e76fb6b154a4
TSUHD <x42hp@gmx.de>
2025-01-03 16:40:45 +0100

    Add files via upload

---
+++ Aufgaben über Weihnachten TS.py
@@ -0,0 +1,7 @@
+print("hello world")
+a= int(input("Hast du schon alle Aufgaben gemacht? "))
+if 5 <= a <=5 :
+  print(f" Super, genieß den Tag. ")
+if a < 5:
+    print("Dann mach sie bevor es zu knapp wird")
+
